a10ea1a77b7fcd31a1b623ee4cff913224c481a8
barbaroscg <barb.ciga@gmail.com>
2024-01-21 13:33:35 +0300

    Repository Patterne Geçiliyor

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.Data.Concrete.EfCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BlogApp.Controllers
+{
+    
+    public class PostsController : Controller
+    {
+        private readonly BlogContext _context;
+        public PostsController(BlogContext context)
+        {
+            _context = context;
+            
+        }
+       
+        public IActionResult Index()
+        {
+            //var model = _context.Posts.ToList();
+            return View( _context.Posts.ToList());
+        }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using BlogApp.Data.Concrete.EfCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BlogContext>(options => {
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("sql_connection");
+    options.UseSqlite(connectionString);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+SeedData.TestVerileriniDoldur(app);
+
+
+// app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();
