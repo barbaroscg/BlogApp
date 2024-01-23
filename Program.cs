b10a99be678330703be6c5a45fec54920113cfa9
barbaroscg <barb.ciga@gmail.com>
2024-01-23 21:56:57 +0300

    routing-design

--- Program.cs
+++ Program.cs
@@ -21,6 +21,24 @@ SeedData.TestVerileriniDoldur(app);
 
 
 // app.MapGet("/", () => "Hello World!");
-app.MapDefaultControllerRoute();
+// app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "post_details",
+    pattern: "posts/{url}",
+    defaults: new {controller = "Posts", action="Details"}
+);
+
+app.MapControllerRoute(
+    name: "posts_by_tag",
+    pattern: "posts/tag/{tag}",
+    defaults: new {controller = "Posts", action="Index"}
+);
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Posts}/{action=Index}/{id?}"
+);
+
+
 
 app.Run();
